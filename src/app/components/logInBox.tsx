91c5932589683c2deee990b0cf425e74770cc2fb
Draimonox <Draimonox@gmail.com>
2024-09-24 00:16:18 -0400

    rebuild

--- src/app/components/logInBox.tsx
+++ src/app/components/logInBox.tsx
@@ -1,153 +1,159 @@
-// if user creating new account, push new account to DataBase
-// if user has account, you want to GET from data base
+// // if user creating new account, push new account to DataBase
+// // if user has account, you want to GET from data base
 
-"use client";
+// "use client";
 
-import { useToggle, upperFirst } from "@mantine/hooks";
-import { isEmail, useForm } from "@mantine/form";
-import {
-  TextInput,
-  PasswordInput,
-  Text,
-  Paper,
-  Group,
-  PaperProps,
-  Button,
-  Checkbox,
-  Anchor,
-  Stack,
-  Container,
-} from "@mantine/core";
-import { Prisma } from "@prisma/client";
+// import { useToggle, upperFirst } from "@mantine/hooks";
+// import { isEmail, useForm } from "@mantine/form";
+// import {
+//   TextInput,
+//   PasswordInput,
+//   Text,
+//   Paper,
+//   Group,
+//   PaperProps,
+//   Button,
+//   Checkbox,
+//   Anchor,
+//   Stack,
+//   Container,
+// } from "@mantine/core";
+// import { Prisma } from "@prisma/client";
 
-export function AuthenticationForm(props: PaperProps) {
-  const [type, toggle] = useToggle(["login", "register"]);
-  const form = useForm({
-    initialValues: {
-      email: "",
-      name: "",
-      password: "",
-      terms: true,
-    },
+// export function AuthenticationForm(props: PaperProps) {
+//   const [type, toggle] = useToggle(["login", "register"]);
+//   const form = useForm({
+//     initialValues: {
+//       email: "",
+//       name: "",
+//       password: "",
+//       terms: true,
+//     },
 
-    validate: {
-      email: (val) => (/^\S+@\S+$/.test(val) ? null : "Invalid email"),
-      password: (val) =>
-        val.length <= 6
-          ? "Password should include at least 6 characters"
-          : null,
-    },
-  });
-  async function registerUser() {
-    try {
-      const res = await fetch("../api/register", {
-        method: "POST",
-        headers: {
-          "Content=Type": "application/json",
-        },
-        body: JSON.stringify({
-          User: {
-            id: Date.now().toString(),
-            createdAt: new Date().toISOString(),
-            email: form.setFieldValue,
-            name: form.setFieldValue.name,
-            password: PasswordInput,
-          },
-        }),
-      });
-      const data = await res.json();
-      console.log(data);
-    } catch (err) {
-      console.error(err);
-    }
+//     validate: {
+//       email: (val) => (/^\S+@\S+$/.test(val) ? null : "Invalid email"),
+//       password: (val) =>
+//         val.length <= 6
+//           ? "Password should include at least 6 characters"
+//           : null,
+//     },
+//   });
+//   async function registerUser() {
+//     try {
+//       const res = await fetch("../api/register", {
+//         method: "POST",
+//         headers: {
+//           "Content=Type": "application/json",
+//         },
+//         body: JSON.stringify({
+//           User: {
+//             id: Date.now().toString(),
+//             createdAt: new Date().toISOString(),
+//             email: form.setFieldValue,
+//             name: form.setFieldValue.name,
+//             password: PasswordInput,
+//           },
+//         }),
+//       });
+//       const data = await res.json();
+//       console.log(data);
+//     } catch (err) {
+//       console.error(err);
+//     }
 
-    return (
-      <Container
-        size="xs"
-        px="xs"
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          minHeight: "90vh",
-        }}
-      >
-        <Paper radius="md" p="md" withBorder {...props}>
-          <Text size="lg" fw={500}>
-            Welcome to the free Todo App, {type} with
-          </Text>
+//     return (
+//       <Container
+//         size="xs"
+//         px="xs"
+//         style={{
+//           display: "flex",
+//           justifyContent: "center",
+//           alignItems: "center",
+//           minHeight: "90vh",
+//         }}
+//       >
+//         <Paper radius="md" p="md" withBorder {...props}>
+//           <Text size="lg" fw={500}>
+//             Welcome to the free Todo App, {type} with
+//           </Text>
 
-          <form onSubmit={form.onSubmit(() => {})}>
-            <Stack>
-              {type === "register" && (
-                <TextInput
-                  label="Name"
-                  placeholder="Your name"
-                  value={form.values.name}
-                  onChange={(event) =>
-                    form.setFieldValue("name", event.currentTarget.value)
-                  }
-                  radius="md"
-                />
-              )}
+//           <form onSubmit={form.onSubmit(() => {})}>
+//             <Stack>
+//               {type === "register" && (
+//                 <TextInput
+//                   label="Name"
+//                   placeholder="Your name"
+//                   value={form.values.name}
+//                   onChange={(event) =>
+//                     form.setFieldValue("name", event.currentTarget.value)
+//                   }
+//                   radius="md"
+//                 />
+//               )}
 
-              <TextInput
-                required
-                label="Email"
-                placeholder="hello@mantine.dev"
-                value={form.values.email}
-                onChange={(event) =>
-                  form.setFieldValue("email", event.currentTarget.value)
-                }
-                error={form.errors.email && "Invalid email"}
-                radius="md"
-              />
+//               <TextInput
+//                 required
+//                 label="Email"
+//                 placeholder="hello@mantine.dev"
+//                 value={form.values.email}
+//                 onChange={(event) =>
+//                   form.setFieldValue("email", event.currentTarget.value)
+//                 }
+//                 error={form.errors.email && "Invalid email"}
+//                 radius="md"
+//               />
 
-              <PasswordInput
-                required
-                label="Password"
-                placeholder="Your password"
-                value={form.values.password}
-                onChange={(event) =>
-                  form.setFieldValue("password", event.currentTarget.value)
-                }
-                error={
-                  form.errors.password &&
-                  "Password should include at least 6 characters"
-                }
-                radius="md"
-              />
+//               <PasswordInput
+//                 required
+//                 label="Password"
+//                 placeholder="Your password"
+//                 value={form.values.password}
+//                 onChange={(event) =>
+//                   form.setFieldValue("password", event.currentTarget.value)
+//                 }
+//                 error={
+//                   form.errors.password &&
+//                   "Password should include at least 6 characters"
+//                 }
+//                 radius="md"
+//               />
 
-              {type === "register" && (
-                <Checkbox
-                  label="I accept terms and conditions"
-                  checked={form.values.terms}
-                  onChange={(event) =>
-                    form.setFieldValue("terms", event.currentTarget.checked)
-                  }
-                />
-              )}
-            </Stack>
+//               {type === "register" && (
+//                 <Checkbox
+//                   label="I accept terms and conditions"
+//                   checked={form.values.terms}
+//                   onChange={(event) =>
+//                     form.setFieldValue("terms", event.currentTarget.checked)
+//                   }
+//                 />
+//               )}
+//             </Stack>
 
-            <Group justify="space-between" mt="xl">
-              <Anchor
-                component="button"
-                type="button"
-                color="dimmed"
-                onClick={() => toggle()}
-                size="xs"
-              >
-                {type === "register"
-                  ? "Already have an account? Login"
-                  : "Don't have an account? Register"}
-              </Anchor>
-              <Button type="submit" radius="xl">
-                {upperFirst(type)}
-              </Button>
-            </Group>
-          </form>
-        </Paper>
-      </Container>
-    );
-  }
-}
+//             <Group justify="space-between" mt="xl">
+//               <Anchor
+//                 component="button"
+//                 type="button"
+//                 color="dimmed"
+//                 onClick={() => toggle()}
+//                 size="xs"
+//               >
+//                 {type === "register"
+//                   ? "Already have an account? Login"
+//                   : "Don't have an account? Register"}
+//               </Anchor>
+//               <Button type="submit" radius="xl">
+//                 {upperFirst(type)}
+//               </Button>
+//             </Group>
+//           </form>
+//         </Paper>
+//       </Container>
+//     );
+//   }
+// }
+
+import React from "react";
+
+export const logInBox = () => {
+  return <div>logInBox</div>;
+};
