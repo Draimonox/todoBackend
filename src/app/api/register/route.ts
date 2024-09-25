415770331a0983b6b87678b2506cc911f5e4c3a3
Draimonox <Draimonox@gmail.com>
2024-09-25 00:35:09 -0400

    build 2

--- src/app/api/register/route.ts
+++ src/app/api/register/route.ts
@@ -1,45 +1,45 @@
-import { PrismaClient } from "@prisma/client";
-import { NextResponse } from "next/server";
+// import { PrismaClient } from "@prisma/client";
+// import { NextResponse } from "next/server";
 
-const prisma = new PrismaClient();
+// const prisma = new PrismaClient();
 
-export async function POST(request: Request) {
-  try {
-    const { userInfo } = await request.json();
+// export async function POST(request: Request) {
+//   try {
+//     const { userInfo } = await request.json();
 
-    // Validate input data
-    if (
-      !userInfo ||
-      !userInfo.id ||
-      !userInfo.name ||
-      !userInfo.email ||
-      !userInfo.password ||
-      !userInfo.dateCreated
-    ) {
-      return NextResponse.json(
-        { error: "Invalid input data" },
-        { status: 400 }
-      );
-    }
+//     // Validate input data
+//     if (
+//       !userInfo ||
+//       !userInfo.id ||
+//       !userInfo.name ||
+//       !userInfo.email ||
+//       !userInfo.password ||
+//       !userInfo.dateCreated
+//     ) {
+//       return NextResponse.json(
+//         { error: "Invalid input data" },
+//         { status: 400 }
+//       );
+//     }
 
-    const newUser = await prisma.user.create({
-      data: {
-        id: userInfo.id,
-        createdAt: userInfo.dateCreated,
-        email: userInfo.email,
-        name: userInfo.name,
-        password: userInfo.password,
-      },
-    });
+//     const newUser = await prisma.user.create({
+//       data: {
+//         id: userInfo.id,
+//         createdAt: userInfo.dateCreated,
+//         email: userInfo.email,
+//         name: userInfo.name,
+//         password: userInfo.password,
+//       },
+//     });
 
-    return NextResponse.json(newUser, { status: 201 });
-  } catch (err) {
-    console.error("Error creating user:", err);
-    return NextResponse.json(
-      { error: "Internal Server Error", details: (err as Error).message },
-      { status: 500 }
-    );
-  } finally {
-    await prisma.$disconnect();
-  }
-}
+//     return NextResponse.json(newUser, { status: 201 });
+//   } catch (err) {
+//     console.error("Error creating user:", err);
+//     return NextResponse.json(
+//       { error: "Internal Server Error", details: (err as Error).message },
+//       { status: 500 }
+//     );
+//   } finally {
+//     await prisma.$disconnect();
+//   }
+// }
